646f41d7c7fdfbbbb1a0f929f3894e45a26b0238
Realest-ice <lhordfendycool999@gmail.com>
2025-11-15 14:45:12 +0100

    Add sendOtp route to auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,5 +4,6 @@ const { requestOtp, verifyOtp } = require('../controllers/authController');
 
 router.post('/request-otp', requestOtp);
 router.post('/verify-otp', verifyOtp);
+router.post("/send-otp", sendOtp);
 
 module.exports = router;
