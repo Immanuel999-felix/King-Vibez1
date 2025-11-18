f30cb21381878ac013dfa2f2b1deca06ff876706
Realest-ice <lhordfendycool999@gmail.com>
2025-11-18 16:53:36 +0100

    Add Next.js configuration file

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { unoptimized: true } // change for production when you add domains
+};
+
+module.exports = nextConfig;
