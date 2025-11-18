470af4fd315612cd67169671716a64feae5c3262
Realest-ice <lhordfendycool999@gmail.com>
2025-11-18 08:55:47 +0100

    Integrate admin creation on MongoDB connection
    Added automatic admin creation after MongoDB connection.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const createAdmins = require('./createAdmins'); // import the file we made
 
 module.exports = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
+
+    // AUTO CREATE ADMINS
+    await createAdmins();
+    console.log("Admin setup complete");
+
   } catch (err) {
     console.error("Mongo error:", err);
     process.exit(1);
